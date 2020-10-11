916ef56e1f5b7a227c9457c123a48057c6b689f8
Azenga Kevin <azenga.kevin7@gmail.com>
2020-10-11 18:03:32 +0300

    Wip, successfully added and tried manually testing resources

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private AnchorPane rootPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ImageView imageView = new ImageView(new Image(getClass().getResource("/assets/board.png").toExternalForm()));
+
+        rootPane.getChildren().add(imageView);
+        imageView.toBack();
+
+
+    }
 }
